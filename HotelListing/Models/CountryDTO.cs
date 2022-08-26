1a73714d27bd9afd05745852e4d86e193d2ef46e
Sk <flubzd@gmail.com>
2022-08-26 18:49:02 +0200

    Added POST, PUT and DELETE endpoints to Hotels and Countries.

--- HotelListing/Models/CountryDTO.cs
+++ HotelListing/Models/CountryDTO.cs
@@ -15,6 +15,14 @@ namespace HotelListing.Models
         [StringLength(maximumLength: 2, ErrorMessage = "Short country name is too long")]
         public string ShortName { get; set; }
     }
+
+    public class UpdateCountryDTO : CreateCountryDTO
+    {
+        public IList<CreateHotelDTO> Hotels
+        {
+            get; set;
+        }
+    }
     public class CountryDTO : CreateCountryDTO
     {
         public int Id { get; set; }
